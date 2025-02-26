f6724efffed2a84ef9efbc86828ae7a0dccb9268
eugpoon <eug.poon@gmail.com>
2025-02-25 22:15:57 -0800

    updated setup

--- setup.py
+++ setup.py
@@ -1,20 +1,16 @@
-import os
-import calendar
+import os, calendar, warnings
 from typing import List, Dict, Tuple
 from collections import defaultdict
-
+from functools import reduce
 import pandas as pd
 import numpy as np
 import operator
-import warnings
-
 warnings.filterwarnings('ignore')
 
 
 global CENTER, EVENT, MONTHS
 CENTER, EVENT, MONTHS = '', '', []
 
-# Initialization
 DATA_PATH = 'compound'
 month_dict = {m: calendar.month_name[m].upper()[0] for m in range(1, 13)}
 HIST = (1961, 1990) # Historical range
@@ -27,22 +23,23 @@ COMP_OPS = { # Comparative operators
 }
 
 
-def get_files() -> List[str]:
+def get_files():
     '''Returns list of filenames in a directory that contain a specific string'''
     return [os.path.join(DATA_PATH, f) for f in os.listdir(DATA_PATH) 
             if CENTER in f and f.endswith('.csv') and TEMPORAL_RES in f
             and any(v in f for v in VARIABLES)]
 
+
 def validate_file_path(file_path: str) -> None:
     '''Validate if the file path exists.'''
     if not os.path.exists(file_path):
-        raise FileNotFoundError(f'The file {file_path} does not exist.')
+        raise FileNotFoundError(f'{file_path} does not exist.')
 
-def preprocess_file(filename: str, is_hist: bool, percentiles: Dict[str, Tuple[str, float]]) -> pd.DataFrame:
+
+def preprocess_file(filename, is_hist, percentiles=None):
     '''Returns preprocessed DataFrame based on CSV file type (historical or SSP)'''
     validate_file_path(filename)
-    
-    name = filename[:-4].split('_')  # Extract variable name
+    name = filename[:-4].split('/')[-1].split('_')
     
     df = pd.read_csv(filename).rename(columns={'Unnamed: 0': 'date'})
     df['date'] = pd.to_datetime(df['date'])
@@ -63,99 +60,62 @@ def preprocess_file(filename: str, is_hist: bool, percentiles: Dict[str, Tuple[s
         df = df[(HIST[0] <= df.index.year) & (df.index.year <= HIST[1])]
         return pd.DataFrame({f'{name[1]}_{op}_{p}': df.quantile(p)})
     else: 
-        df = df[df.index.month.isin(MONTHS)]     
-        return ('_'.join(name[1:3]), df.add_suffix(f'_{name[1]}'))
+        df = df[df.index.month.isin(MONTHS)]
+        df.insert(0, 'ssp', name[2])
+        return '_'.join(name[1:3]), df
 
 
-def add_compound_flag(hist_df: pd.DataFrame, ssp_df: pd.DataFrame, percentiles: Dict[str, Tuple[str, float]]) -> pd.DataFrame:
-    '''Returns SSP dataframe with added compound flag based on historical percentiles'''
-    for model in hist_df.index:
-        flag = []
-        for var, (op, p) in percentiles.items():
-            # Check if ssp values exceed historical percentiles
-            perc = hist_df[f'{var}_{op}_{p}'].loc[model]
-            ssp_values = ssp_df[f'{model}_{var}'].to_list()
-            
-            # Apply the comparison function
-            ssp_df[f'{model}_{var}_{op}_{p}'] = COMP_OPS[op](ssp_values, perc)
-            flag.append(ssp_df[f'{model}_{var}_{op}_{p}'].to_list())
-        
-            # Calculate diff
-            # ssp_df[f'{model}_{var}_diff'] = ssp_df[f'{model}_{var}'] - perc
-        
-        ssp_df[f'{model}_compound'] = np.all(flag, axis=0)
-    return ssp_df
-
-def setup_thresholds(event: str) -> List[Dict[str, Tuple[str, float]]]:
+def setup_thresholds(event):
     '''Set up thresholds for different severity levels of compound events.'''    
     if event == 'CDHE':
         pr_op, tm_op = '<', '>'
-        return [
-            {'pr': (pr_op, 0.5), 'tasmax': (tm_op, 0.90)},  # base
-            {'pr': (pr_op, 0.5), 'tasmax': (tm_op, 0.75)},  # least severe
-            {'pr': (pr_op, 0.4), 'tasmax': (tm_op, 0.80)},
-            {'pr': (pr_op, 0.3), 'tasmax': (tm_op, 0.85)},
-            {'pr': (pr_op, 0.2), 'tasmax': (tm_op, 0.90)},
-            {'pr': (pr_op, 0.1), 'tasmax': (tm_op, 0.95)},  # most severe
-        ]
-        
+        return [{'pr': (pr_op, p), 'tasmax': (tm_op, q)} for p, q in 
+                # base; least --> most severe
+                zip([0.50, 0.50, 0.40, 0.30, 0.20, 0.10],  # pr
+                    [0.90, 0.75, 0.80, 0.85, 0.90, 0.95])] #tm  
     elif event == 'CWHE':
         pr_op, tm_op = '>', '>'
-        return [
-            {'pr': (pr_op, 0.5), 'tasmax': (tm_op, 0.90)},  # base
-            {'pr': (pr_op, 0.5), 'tasmax': (tm_op, 0.75)},  # least severe
-            {'pr': (pr_op, 0.6), 'tasmax': (tm_op, 0.80)},
-            {'pr': (pr_op, 0.7), 'tasmax': (tm_op, 0.85)},
-            {'pr': (pr_op, 0.8), 'tasmax': (tm_op, 0.90)},
-            {'pr': (pr_op, 0.9), 'tasmax': (tm_op, 0.95)},  # most severe
-        ]
-        
+        return [{'pr': (pr_op, p), 'tasmax': (tm_op, q)} for p, q in 
+                # base; least --> most severe
+                zip([0.50, 0.50, 0.60, 0.70, 0.80, 0.90],  # pr
+                    [0.90, 0.75, 0.80, 0.85, 0.90, 0.95])] # tm
     else:
         raise ValueError('Invalid event type')
-        
-def process_data(files: Dict[str, List[str]], percentiles: Dict[str, Tuple[str, float]]) -> Tuple[pd.DataFrame, Dict[str, pd.DataFrame]]:
-    '''Process historical and SSP climate data files'''
-    # Process historical files and merge into a single DataFrame
-    hist_df = pd.concat([preprocess_file(f, True, percentiles) 
-                         for f in files['historical']], axis=1).dropna()
 
-    # Process SSP files and store in a dictionary
-    ssp_dfs = {name: df for name, df in (preprocess_file(f, False, percentiles) 
-                                         for f in files['ssp'])}
 
-    # Merge all SSP data from the same scenario
-    grouped_data = defaultdict(list)
-    for key, df in ssp_dfs.items():
-        grouped_data[key.split('_')[-1]].append(df)
-
-    # Add flag to indicate compound events    
-    ssp_dfs = {ssp: add_compound_flag(hist_df, pd.concat(dfs, axis=1), percentiles) 
-               for ssp, dfs in grouped_data.items()}
+def add_compound_flag(hist_df, ssp_df, percentiles):
+    '''Returns SSP dataframe with added compound flag based on historical percentiles'''
+    df = ssp_df[['date', 'ssp']]
+    for model in hist_df.index:
+        flag = []
+        for var, (op, p) in percentiles.items():
+            perc = hist_df[f'{var}_{op}_{p}'].loc[model] # percentile
+            ssp_values = ssp_df[f'{model}_{var}'].to_list() # projected
+            df[f'{model}_{var}_{op}_{p}'] = COMP_OPS[op](ssp_values, perc) # compare
+            flag.append(df[f'{model}_{var}_{op}_{p}'].to_list())
+        df[f'{model}_compound'] = np.all(flag, axis=0)
+    return df
     
-    return hist_df, ssp_dfs
 
-
-def max_consecutive(s: pd.Series) -> int:
+def max_consecutive(s):
     '''Calculate maximum consecutive True values in a series.'''
     return (s * (s.groupby((s != s.shift()).cumsum()).cumcount() + 1)).max()
 
 
-def get_data(dfs: Dict[str, pd.DataFrame], suffix: str, agg: str, duration: bool = False) -> Dict[str, pd.DataFrame]:
-    '''Aggregate data from DataFrames based on specified criteria'''
-    dict_df = {}
-    for m, df in dfs.items():
-        df_ = df.filter(regex=suffix)
-        df_.index = pd.to_datetime(df_.index, format='%Y')
-        if duration:
-            df_ = df_.groupby(df_.index.year).apply(lambda x: x.apply(max_consecutive))
-        else:
-            df_ = df_.groupby(df_.index.year).agg(agg)
-        dict_df[m] = df_
-    return dict_df
+def group_data(df, suffix):
+    '''Group data based on specified criteria'''
+    df_ = df.copy()
+    df_.date = df_.date.dt.year
+    df_ = (df_.set_index(['ssp', 'date']).filter(regex=suffix)
+           .groupby(['ssp', 'date']))
+    return df_
+
+def get_common_columns(dfs):
+    return reduce(lambda x, y: x.intersection(y), (df.columns for df in dfs))
 
 
-def main(center: str, event: str, months: List[int]) -> Tuple[Dict, Dict, Dict, Dict, Dict, Dict]:
-    '''Main function to process climate data'''
+def main(center, event, months):
+
     global CENTER, EVENT, MONTHS
     CENTER, EVENT, MONTHS = center, event, months
 
@@ -163,17 +123,60 @@ def main(center: str, event: str, months: List[int]) -> Tuple[Dict, Dict, Dict, 
 
     files = {key: [f for f in sorted(get_files()) if key in f] for key in ['historical', 'ssp']}
     
-    dfs, hist, frequency, duration, pr, tm = {}, {}, {}, {}, {}, {}
+    # Process SSP files
+    ssp_dfs = {name: df for name, df in (preprocess_file(f, False, None) for f in files['ssp'])}
+    results = defaultdict(lambda: pd.DataFrame())
+    for k, df in ssp_dfs.items():
+        ssp = k.split('_')[0]
+        results[ssp] = pd.concat([results[ssp], df], axis=0)
+        
+    cols = get_common_columns(list(results.values()))
+
+    ssp_dfs = None
+    for key, df in results.items():
+        df = df[cols].reset_index()
+        df.rename(columns={c: f'{c}_{key}' for c in df.columns 
+                           if c not in ['date', 'ssp']}, inplace=True)
+        ssp_dfs = df if ssp_dfs is None else ssp_dfs.merge(df, on=['date', 'ssp'])
+
+    comp, hist, results = {}, {}, {}
     for threshold in thresholds:
         name = '_'.join([f'{v}{c}{p}' for v, (c, p) in threshold.items()])
-        hist[name], dfs[name] = process_data(files, threshold)
-        
-        frequency[name] = get_data(dfs[name], f'_compound$', 'sum', False)
-        duration[name] = get_data(dfs[name], f'_compound$', 'mean', True)
-        pr[name] = get_data(dfs[name], f'_pr$', 'mean', False)
-        tm[name] = get_data(dfs[name], f'_tasmax$', 'mean', False)
-        
-    return dfs, hist, frequency, duration, pr, tm
+
+        hist[name] = pd.concat([preprocess_file(f, True, threshold) 
+                         for f in files['historical']], axis=1).dropna() 
+        comp[name] = add_compound_flag(hist[name], ssp_dfs, threshold)
+
+        comp_ = group_data(comp[name], f'_compound$')
+        results[name] = comp_.sum().add_suffix('_total')
+        results[name] = pd.concat(
+            [comp_.apply(lambda x: x.apply(max_consecutive)).add_suffix('_duration'), 
+             results[name]], axis=1)
+    pr = group_data(ssp_dfs, f'_pr$').mean()
+    tm = group_data(ssp_dfs, f'_tasmax$').mean()
+
+
+    return ssp_dfs, hist, comp, results, pr, tm
 
 if __name__ == '__main__':
-    dfs, hist, frequency, duration, pr, tm = main(center = 'LARC', event='CDHE', months='[6, 7, 8]')
+    ssp_dfs, hist, comp, results, pr, tm = main(center = 'LARC', event='CDHE', months='[6, 7, 8]')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
