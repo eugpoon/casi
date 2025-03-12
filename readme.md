e9b153c26f5e0a8ea47c94780370dce0e55f723b
eugpoon <eug.poon@gmail.com>
2025-03-12 09:43:28 -0700

    v2 spi calc processes whole df; changes to plot class

--- readme.md
+++ readme.md
@@ -1,8 +1,8 @@
 # V2 (in progess)
 ## TODO:
-- Look at other time scale: 3, 6, 9 months using daily data
+- Look at other time scale: (7, 14) days and (3, 6, 9) months using daily data
+- Move threshold dict to main notebook
 - Apply to other centers
-- Monthly looks questionable with only JJA --> ignore for now
 
 ## Completed
 - Use >90F (32.22C) for temperature threshold to capture extreme heat waves
@@ -13,37 +13,34 @@
 	- total compound event: a compound day that is part of a sequence of more than one consecutive days
     - total compound event sequences: a sequence of consecutive compound events with size>1
     - average duration of compound events
-- Add standardized precipitation index
-	- +spi (wet), -spi (dry), magnitude (severity)
-    - shorter time scale = short term drought
+- Replace precipitation with standardized precipitation index (SPI)
 - Current implementation uses daily spi and tasmax values
-- Change SPI caluculations
-    - Fit gamma dist on 1981-2020 plus n days/months before
-    - Calculate spi for 2021-2100
-    - Include rolling sums from n days before
 - Add variable comparison plot per ssp
 
-### SPI general calc:
-- Add options
-    - freq = ['D', 'M']
-    - scale = #
-- If freq == 'M': get monthly pr files
-- If freq == 'D': get daily pr files
-- Concat 3 dfs of pr data:
-    - [historical, ssp126]
-    - [historical, ssp245]
-    - [historical, ssp370]
-- Calculate SPI for each model using "from standard_precip.spi import SPI"
-- Extract results JJA for 2021-2100 for spi and tasmax
-    - Filter by common model/column names
-    - Concat spi and tasmax axis=0
-    - Concat ssp's axis=1
-    - Determine if compound
-    - Output 1 df per threshold combo
-- https://github.com/e-baumer/standard_precip/blob/master/standard_precip/base_sp.py
+## Modified SPI Calculations:
+- Source: https://github.com/e-baumer/standard_precip/blob/master/standard_precip/base_sp.py
+- Setup: dataframe of precipitation values; rows are days/months; columns are models
+    - freq = ['D', 'M']; monthly looks questionable with only JJA --> ignore for now
+    - scale = #; shorter time scale = short term drought
+    - If freq == 'M': get monthly pr files
+    - If freq == 'D': get daily pr files
+- Calculate rolling sum of \<scale\> window size for accumulated precipitation
+- Fit a gamma distribution for each day of the year using historical values (1981-2020)
+    - Values: rolling sums for current day plus \<scale\> days prior
+- Calculate SPI for 2021-2100 based on gamma distribution
+    - +spi (wet), -spi (dry), magnitude (severity)
 
-### Problem:
-#### Missing values: issue with SPI calculation
+## Determine if compound:
+- Extract results JJA for 2021-2100 for spi and tasmax
+- Establish threshold conditions for each variable
+- Filter by common model/column names
+- Concat spi and tasmax axis=0
+- Concat ssp's axis=1
+- Determine if threshold conditions are met
+- Output 1 df per threshold combo
+
+## Problem:
+### Missing values: issue with SPI calculation
 - Handle missing data to calculate spi (last paragraph) [link](https://www.droughtmanagement.info/literature/WMO_standardized_precipitation_index_user_guide_en_2012.pdf#page=9)
 - 1 row of missing pr results in \<scale\> rows of missing spi
 - Columns with a lot of nan's may result in no spi (KACE-1-0-G for LARC)
@@ -53,7 +50,7 @@
         - [INM-CM4-8, INM-CM5-0, NorESM2-MM, NorESM2-LM, GFDL-ESM4, GISS-E2-1-G, FGOALS-g3, BCC-CSM2-MR, CMCC-ESM2, CESM2]
 - How to handle missing values? 
     - Check original code
-    - Options: interpolation by time, multivariate imputation (slow), back/front fill, ...
+    - Options: interpolation, multivariate imputation (slow), back/front fill, ...
 - Solution: interpolation by linear (df sorted by time)
 
 ## 02/26/25 meeting notes
--- v2/plot.py
+++ v2/plot.py
@@ -1,4 +1,3 @@
-# Imports
 import os
 import calendar
 import pandas as pd
@@ -11,8 +10,9 @@ from plotly.subplots import make_subplots
 
 warnings.filterwarnings('ignore')
 
+# Set plot defaults
 plt.rcParams['figure.figsize'] = (15, 4)
-plt.rcParams['figure.dpi'] = 300 # 600
+plt.rcParams['figure.dpi'] = 150 # 300, 600
 plt.rcParams['font.size'] = 10
 plt.rcParams['figure.titlesize'] = 15
 plt.rcParams['axes.linewidth'] = 0.1
@@ -20,167 +20,164 @@ plt.rcParams['patch.linewidth'] = 0
 plt.rcParams['axes.grid'] = True
 plt.rcParams['grid.linewidth'] = 0.2
 
+class Plot:
+    ##################################################
+    ##                   General                    ##
+    ##################################################
 
-colors = px.colors.qualitative.Plotly
-month_dict = {m: calendar.month_name[m].upper()[0] for m in range(1, 13)}
+    def __init__(self, center_, months_, title_var_, var_):
+        self.center = center_
+        self.months = months_
+        self.title_var = title_var_
+        self.var = var_
+        self.colors = px.colors.qualitative.Plotly
+        self.month_dict = {m: calendar.month_name[m].upper()[0] for m in range(1, 13)}
 
-##################################################
-##                   General                    ##
-##################################################
+        self.month_title = '(' + ''.join(self.month_dict[month] for month in months_) + ')'
+        self.title = f'{center_} - {title_var_} Per Year {self.month_title} Across CMIP6 Models'
 
-def initialize(center_, months_, title_var_, var_, plotly_=False):
-    global title, title_var, var, plotly
-    month_title = '(' + ''.join(month_dict[month] for month in months_) + ')'
-    title = f'{center_} - {title_var_} Per Year {month_title} Across CMIP6 Models'
-    title_var = title_var_
-    var = var_
-    plotly = plotly_
+    def check_df(self, df):
+        '''Filter dataframe based on variable.'''
+        df = df.filter(regex=self.var)
+        if df.empty:
+            raise ValueError('Empty dataframe')
+        return df
     
-def check_df(df):
-    df = df.filter(regex=var)
-    if df.empty:
-        raise ValueError('Empty dataframe')
-    return df
+    ##################################################
+    ##                   KDE Plot                   ##
+    ##################################################
 
-##################################################
-##                   KDE Plot                   ##
-##################################################
+    def dist_sns(self, df, agg='mean', alpha=1, threshold=''):
+        '''Plot distribution using seaborn.'''
+        df = self.check_df(df)
+        for i, ssp in enumerate(df.index.get_level_values('ssp').unique()):
+            sns.kdeplot(data=df.loc[ssp].agg(agg, axis=1), label=f'{ssp}_{agg}', 
+                        color=self.colors[i % len(self.colors)], linewidth=0.5, alpha=alpha)
+        plt.title(f'{self.title} {threshold}')
+        plt.xlabel(self.title_var)
+        plt.legend(loc='upper left', bbox_to_anchor=(1, 1))
+        plt.show()
 
-def plot_dist(df, agg='mean', alpha=1, threshold=''):
-    df = check_df(df)
-    for i, ssp in enumerate(df.index.get_level_values('ssp').unique()):
-        sns.kdeplot(data=df.loc[ssp].agg(agg, axis=1), label=f'{ssp}_{agg}', 
-                    color=colors[i % len(colors)], linewidth=0.5, alpha=alpha)
-    plt.title(f'{title} {threshold}')
-    plt.xlabel(title_var)
-    plt.legend(loc='upper left', bbox_to_anchor=(1, 1))
-    plt.show()
+    ##################################################
+    ##                  Time Series                 ##
+    ##################################################
 
-##################################################
-##                  Time Series                 ##
-##################################################
-
-def plot_ts(df, agg='mean', alpha=1, threshold=''):
-    df = check_df(df)
-    if plotly:
+    def ts_plotly(self, df, agg='mean', alpha=1, threshold=''):
+        '''Plot time series using plotly.'''
+        df = self.check_df(df)
         fig = go.Figure()
         for i, ssp in enumerate(df.index.get_level_values('ssp').unique()):
             data = df.loc[ssp]
-            mean_data = data.agg(agg, axis=1)
-            p10 = data.quantile(0.1, axis=1)
-            p90 = data.quantile(0.9, axis=1)
-             # Add error range (with no legend entry)
-            fig.add_trace(go.Scatter(x=mean_data.index.tolist() + mean_data.index[::-1].tolist(),
-                                     y=p90.tolist() + p10[::-1].tolist(),
-                                     fill='toself', fillcolor=colors[i],
-                                     line=dict(color='rgba(255,255,255,0)'), hoverinfo='skip',
-                                     showlegend=False, legendgroup=ssp, opacity=0.2))
-            fig.add_trace(go.Scatter(x=mean_data.index, y=mean_data, name=ssp, mode='lines',
-                                     line=dict(color=colors[i], width=0.8), opacity=alpha,
-                                     legendgroup=ssp))
+            agg_data, p10, p90 = data.agg(agg, axis=1), data.quantile(0.1, axis=1), data.quantile(0.9, axis=1)
+            
+            # Add error range (with no legend entry)
+            fig.add_trace(go.Scatter(x=agg_data.index.tolist() + agg_data.index[::-1].tolist(),
+                                     y=p90.tolist() + p10[::-1].tolist(), fill='toself',
+                                     fillcolor=self.colors[i], showlegend=False, legendgroup=ssp, opacity=0.2,
+                                     line=dict(color='rgba(255,255,255,0)'), hoverinfo='skip'))
+            
+            fig.add_trace(go.Scatter(x=agg_data.index, y=agg_data, name=ssp, mode='lines', opacity=alpha,
+                                     line=dict(color=self.colors[i], width=0.8), legendgroup=ssp))
 
-        fig.update_layout(title=f'{title} {threshold}', xaxis_title='Year', yaxis_title=title_var,
+        fig.update_layout(title=f'{self.title} {threshold}', xaxis_title='Year', yaxis_title=self.title_var,
                           width=1000, height=300, margin=dict(l=20, r=20, t=30, b=20),
                           paper_bgcolor='white', plot_bgcolor='white',
                           xaxis=dict(showgrid=True, gridcolor='lightgrey', gridwidth=0.1),
                           yaxis=dict(showgrid=True, gridcolor='lightgrey', gridwidth=0.1))
         fig.show()
-    else:
+
+    def ts_sns(self, df, agg='mean', alpha=1, threshold=''):
+        '''Plot time series using seaborn.'''
+        df = self.check_df(df)
+
         for i, ssp in enumerate(df.index.get_level_values('ssp').unique()):
             data = df.loc[ssp].agg(agg, axis=1)
-            sns.lineplot(data=data, label=ssp, color=colors[i], linewidth=0.5, alpha=alpha)
-        plt.title(f'{title} {threshold}')
+            sns.lineplot(data=data, label=ssp, color=self.colors[i], linewidth=0.5, alpha=alpha)
+        plt.title(f'{self.title} {threshold}')
         plt.xlabel('Year')
-        plt.ylabel(title_var)
+        plt.ylabel(self.title_var)
         plt.legend(loc='center left', bbox_to_anchor=(1, 1))
         plt.tight_layout()
         plt.show()
         for i, ssp in enumerate(df.index.get_level_values('ssp').unique()):
             data = df.loc[ssp]
-            agg_data = data.agg(agg, axis=1)
-            p10 = data.quantile(0.1, axis=1)
-            p90 = data.quantile(0.9, axis=1)
+            agg_data, p10, p90 = data.agg(agg, axis=1), data.quantile(0.1, axis=1), data.quantile(0.9, axis=1)
             
-            sns.lineplot(data=agg_data, label=ssp, color=colors[i], linewidth=0.8, alpha=alpha)
-            plt.fill_between(agg_data.index, p10, p90, color=colors[i], alpha=0.1)
-            plt.title(f'{title} {threshold}')
+            sns.lineplot(data=agg_data, label=ssp, color=self.colors[i], linewidth=0.8, alpha=alpha)
+            plt.fill_between(agg_data.index, p10, p90, color=self.colors[i], alpha=0.1)
+            plt.title(f'{self.title} {threshold}')
             plt.xlabel('Year')
-            plt.ylabel(title_var)
+            plt.ylabel(self.title_var)
             plt.legend(loc='center left', bbox_to_anchor=(1, 1))
             plt.tight_layout()
             plt.show()
+            
+    ##################################################
+    ##    Threshold Comparison (1 ssp per plot)     ##
+    ##################################################
 
-##################################################
-##    Threshold Comparison (1 ssp per plot)     ##
-##################################################
-
-def plot_severity(results, ssps, var, agg='mean', alpha=1):
-    if plotly:
-        fig = make_subplots(rows=3, cols=1, shared_xaxes='all', shared_yaxes='all', 
+    def severity_plotly(self, results, ssps, agg='mean', alpha=1):
+        '''Plot severity comparison using plotly.'''
+        fig = make_subplots(rows=len(ssps), cols=1, shared_xaxes='all', shared_yaxes='all', 
                             subplot_titles=ssps, vertical_spacing=0.05)
 
         for i, ssp in enumerate(ssps, start=1):
             for j, (threshold, df) in enumerate(results.items()):
-                data = check_df(df[df.index.get_level_values(0) == ssp])
-                mean_data = data.agg(agg, axis=1)
-                p10 = data.quantile(0.1, axis=1)
-                p90 = data.quantile(0.9, axis=1)
+                data = self.check_df(df[df.index.get_level_values(0) == ssp])
+                agg_data,p10,p90 = data.agg(agg, axis=1), data.quantile(0.1, axis=1), data.quantile(0.9, axis=1)
 
                 # Add error range
                 fig.add_trace(go.Scatter(
-                    x=(mean_data.index.get_level_values(1).tolist() + 
-                       mean_data.index.get_level_values(1)[::-1].tolist()),
+                    x=(agg_data.index.get_level_values(1).tolist() + 
+                       agg_data.index.get_level_values(1)[::-1].tolist()),
                     y=p90.tolist() + p10[::-1].tolist(), showlegend=False, legendgroup=threshold,
-                    fill='toself', fillcolor=colors[j % len(colors)], opacity=0.2, 
+                    fill='toself', fillcolor=self.colors[j % len(self.colors)], opacity=0.2, 
                     line=dict(color='rgba(255,255,255,0)'), hoverinfo='skip'), row=i, col=1)
                 
                 fig.add_trace(go.Scatter(
-                    x=mean_data.index.get_level_values(1), y=mean_data, name=threshold,
-                    line=dict(color=colors[j % len(colors)], width=0.8), mode='lines',
+                    x=agg_data.index.get_level_values(1), y=agg_data, name=threshold,
+                    line=dict(color=self.colors[j % len(self.colors)], width=0.8), mode='lines',
                     opacity=alpha, legendgroup=threshold, showlegend=(i == 1)), row=i, col=1)
 
-        fig.update_layout(title='Severity Comparison: '+title, width=1000, height=200*len(ssps),
+        fig.update_layout(title='Severity Comparison: '+self.title, width=1000, height=200*len(ssps),
                           margin=dict(l=50, r=20, t=120, b=50),
-            paper_bgcolor='white', plot_bgcolor='white', legend_title='Thresholds',
-            legend=dict(orientation='h', yanchor='bottom', y=1.05, xanchor='center', x=0.5)
+                          paper_bgcolor='white', plot_bgcolor='white', legend_title='Thresholds',
+                          legend=dict(orientation='h', yanchor='bottom', y=1.05, xanchor='center', x=0.5)
         )
 
         for i in range(1, len(ssps)+1):
-            fig.update_xaxes(showgrid=True, gridcolor='lightgrey', gridwidth=0.1, row=i, col=1)
+            fig.update_xaxes(showgrid=True, gridcolor='lightgrey', gridwidth=0.1, row=i, col=1, 
+                             title_text='Year' if i == len(ssps) else '')
             fig.update_yaxes(showgrid=True, gridcolor='lightgrey', gridwidth=0.1, row=i, col=1,
-                             title_text=title_var if i == len(ssps)//2 + 1 else '')
-            fig.update_xaxes(title_text='Year' if i == len(ssps) else '', row=i, col=1)
-
+                             title_text=self.title_var if i == len(ssps)//2 + 1 else '')
         fig.show()
 
-    else:
+    def severity_sns(self, results, ssps, agg='mean', alpha=1):
+        '''Plot severity comparison using seaborn.'''
         fig, axes = plt.subplots(len(ssps), 1, figsize=(15, 5*len(ssps)), sharex=True, sharey=True)
-        fig.suptitle('Severity Comparison: '+title, fontsize=16, y=0.95)
+        fig.suptitle('Severity Comparison: '+self.title, fontsize=16, y=0.95)
 
         lines, labels = [], []
 
         for i, ssp in enumerate(ssps):
             ax = axes[i] if len(ssps) > 1 else axes
             for j, (threshold, df) in enumerate(results.items()):
-                data = check_df(df[df.index.get_level_values(0) == ssp])
-                mean_data = data.agg(agg, axis=1)
-                p10 = data.quantile(0.1, axis=1)
-                p90 = data.quantile(0.9, axis=1)
+                data = self.check_df(df[df.index.get_level_values(0) == ssp])
+                agg_data,p10,p90 = data.agg(agg, axis=1), data.quantile(0.1, axis=1), data.quantile(0.9, axis=1)
                 
-                line = sns.lineplot(x=data.index.get_level_values(1), y=mean_data, 
-                                    label=threshold, alpha=alpha, color=colors[j % len(colors)], 
+                line = sns.lineplot(x=data.index.get_level_values(1), y=agg_data, 
+                                    label=threshold, alpha=alpha, color=self.colors[j % len(self.colors)], 
                                     linewidth=0.8, ax=ax)
-                ax.fill_between(mean_data.index.get_level_values(1), p10, p90, 
-                        color=colors[j % len(colors)], alpha=0.1)
+                ax.fill_between(agg_data.index.get_level_values(1), p10, p90, 
+                                color=self.colors[j % len(self.colors)], alpha=0.1)
                 
                 if i == 0:
                     lines.append(line.lines[-1])
                     labels.append(threshold)
 
-            ax.set_title(ssp)
+            ax.set(title=ssp, ylabel=(self.title_var if i == len(ssps)//2 else ''), 
+                   xlabel=('Year' if i == len(ssps)-1 else ''))
             ax.grid(True, color='lightgrey', linewidth=0.2)
-            ax.set_ylabel(title_var if i == len(ssps)//2 else '')
-            ax.set_xlabel('Year' if i == len(ssps)-1 else '')
             ax.legend().remove()
 
         fig.legend(lines, labels, title='Thresholds', bbox_to_anchor=(0.5, 0.98), 
@@ -189,34 +186,76 @@ def plot_severity(results, ssps, var, agg='mean', alpha=1):
         plt.subplots_adjust(top=0.90)
         plt.show()
 
-def plot_all(results, agg='mean', alpha=1):
-    ssps = list(pd.unique([idx[0] for df in results.values() for idx in df.index]))
-    for threshold, df in results.items(): 
-        # print(f'{threshold}\n\n')
-        plot_dist(df, agg, alpha, threshold)
-        plot_ts(df, agg, alpha, threshold)
     
-    plot_severity(results, ssps, var, agg=agg, alpha=alpha)
+    ##################################################
+    ##     Variable Comparison (1 ssp per plot)     ##
+    ##################################################
 
-
-def variable_comp(results, var, metrics, center_, months_, agg='mean', alpha=1):
-    ssps = list(pd.unique([idx[0] for df in results.values() for idx in df.index]))
-    month_title = '(' + ''.join(month_dict[month] for month in months_) + ')'
-    title = f'{center_} - <variable> Per Year {month_title} Across CMIP6 Models'
+    def variable_comp(self, results, var, metrics, agg='mean', alpha=1):
+        '''Plot variable comparison.'''
+        ssps = list(pd.unique([idx[0] for df in results.values() for idx in df.index]))
+        title = f'{self.center} - <variable> Per Year {self.month_title} Across CMIP6 Models'
+        
+        for threshold, df in results.items(): 
+            for ssp in df.index.get_level_values('ssp').unique():
+                for i, col in enumerate(var):
+                    data = df.filter(regex=col).loc[ssp].agg(agg, axis=1)
+                    sns.lineplot(data=data, label=metrics[col], color=self.colors[i], 
+                                 linewidth=0.5, alpha=alpha)
+                plt.title(f'{title}; {ssp}; {threshold}')
+                plt.xlabel('Year')
+                plt.ylabel('')    
+                plt.legend(loc='center left', bbox_to_anchor=(1, 1))
+                plt.tight_layout()
+                plt.show()
     
-    for threshold, df in results.items(): 
-        for ssp in df.index.get_level_values('ssp').unique():
-            for i, col in enumerate(var):
-                data = df.filter(regex=col).loc[ssp].agg(agg, axis=1)
-                sns.lineplot(data=data, label=metrics[col], color=colors[i], 
-                             linewidth=0.5, alpha=alpha)
-            plt.title(f'{title}; {ssp}; {threshold}')
-            plt.xlabel('Year')
-            plt.ylabel('')    
-            plt.legend(loc='center left', bbox_to_anchor=(1, 1))
-            plt.tight_layout()
-            plt.show()
+    def all(self, results, plotly_, agg='mean', alpha=1):
+        '''Plot all types of plots.'''
+        ssps = list(pd.unique([idx[0] for df in results.values() for idx in df.index]))
+        if plotly_:
+            for threshold, df in results.items(): 
+                self.dist_sns(df, agg, alpha, threshold)
+                self.ts_plotly(df, agg, alpha, threshold)
+            self.severity_plotly(results, ssps, agg=agg, alpha=alpha)
+        else:
+            for threshold, df in results.items(): 
+                self.dist_sns(df, agg, alpha, threshold)
+                self.ts_sns(df, agg, alpha, threshold)
+            self.severity_sns(results, ssps, agg=agg, alpha=alpha)
+            
 
 
 
 
+
+
+
+        
+
+
+
+
+
+
+        
+
+
+
+
+
+
+        
+
+
+
+
+
+
+        
+
+
+
+
+
+
+        
